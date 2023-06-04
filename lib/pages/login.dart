65ebb5cad71949a0191fe6f86faf5f9ef6b1a751
Vishnu <vishnunair20012112@gmail.com>
2023-06-04 16:51:07 +0530

    error indications added

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,6 +8,24 @@ class Login extends StatefulWidget{
 class _LoginState extends State<Login> {
   String name="";
   bool change = false;
+  final _formkey=GlobalKey<FormState>();
+
+
+  moveToHome(BuildContext) async{
+    if( _formkey.currentState!.validate()){
+      setState(() {
+        change=true;});
+      await Future.delayed(Duration(milliseconds:1000));
+      await Navigator.pushNamed(context, "/home");
+      setState(() {
+        change=false;
+      });
+    }
+
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -15,41 +33,52 @@ class _LoginState extends State<Login> {
       color: Colors.white,
 
  child: SingleChildScrollView(
-   child: Column(
-     mainAxisSize: MainAxisSize.min,
-      children: [
-       Image.asset("asset/login.png",height: 350),
-       SizedBox(height: 10,),
-       Text('Welcome $name',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical:16,horizontal: 32),
-          child: Column(
-            children: [
-              TextFormField(decoration: InputDecoration(hintText: "Username"),onChanged: (value){name=value;setState(() {
+   child: Form(
+     key: _formkey,
+     child: Column(
+       mainAxisSize: MainAxisSize.min,
+        children: [
+         Image.asset("asset/login.png",height: 350),
+         SizedBox(height: 10,),
+         Text('Welcome $name',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical:16,horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(decoration: InputDecoration(hintText: "Username"),
+                            validator: (value){
+                              if(value!=null &&value.isEmpty){
+                                return "Username cannot be empty";
+                              }
+                               else return null;
+                            },
+                              onChanged: (value){name=value;setState(() {
 
-              });}, ),
+                         });}, ),
 
-              TextFormField(obscureText:true,decoration: InputDecoration(hintText: "Password"),),
-            ],
-          ),
-        ),
-      SizedBox(height: 20,),
-       Material( borderRadius: BorderRadius.circular(24),color: Colors.lightBlue,
-         child: InkWell(onTap: () async {setState(() {
-           change=true;});
-           await Future.delayed(Duration(milliseconds:1000));
-          await Navigator.pushNamed(context, "/home");
-          setState(() {
-            change=false;
-          });
-           },
-           child: AnimatedContainer(duration:Duration(milliseconds: 1000),height: 43,width:change?43:100,
-                          child: change?Icon(Icons.done,color: Colors.white,):Center(child: Text('Login',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)),
+                TextFormField(obscureText:true,decoration: InputDecoration(hintText: "Password"),
+                                validator: (value){
+                                  if(value!=null && value.isEmpty){
+                                    return "Please enter the password!";}
+                                  else if (value!=null && value.length<6){
+                                    return "Password length should be atleast 6 characters.";
+                                  }
+                                    else return null;
+                                },),
+              ],
             ),
+          ),
+        SizedBox(height: 20,),
+         Material( borderRadius: BorderRadius.circular(24),color: Colors.lightBlue,
+           child: InkWell(onTap: () =>moveToHome(context),
+             child: AnimatedContainer(duration:Duration(milliseconds: 1000),height: 43,width:change?43:100,
+                            child: change?Icon(Icons.done,color: Colors.white,):Center(child: Text('Login',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)),
+              ),
+           ),
          ),
-       ),
-      ],
-    ),
+        ],
+      ),
+   ),
  ));
   }
 }
