10d9d083c669e53be5ac8fb17cf90ca1c30dd074
Vishnu <vishnunair20012112@gmail.com>
2023-06-09 22:53:21 +0530

    json added

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,14 +1,30 @@
 
 
 class Catalog  {
-  String id ;
+  static List<Catalog> items = [];
+
+  int id ;
   String name ;
   String descprition ;
   num price ;
   String color ;
   String imageurl ;
-  Catalog(this.id, this.name, this.descprition, this.price, this.color,
-      this.imageurl);
 
+
+  Catalog({required this.id, required this.name, required this.descprition, required this.price, required this.color,
+      required this.imageurl});
+
+ factory Catalog.fromMap(Map<String,dynamic> map){
+   return Catalog(
+       id: map["id"],
+       name: map["name"],
+       descprition: map["descprition"],
+       price: map["price"],
+       color: map["color"],
+       imageurl: map["imageurl"],
+
+
+   );
+ }
 
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,64 +1,60 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:learning/models/catalog.dart';
 
 import '../widgets/drawer.dart';
 
-class Homepage extends StatelessWidget{
+class Homepage extends StatefulWidget{
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    loaddata();
+    print(1);
+    super.initState();
+    loaddata();
+    print(2);
+  }
+
+  loaddata() async{
+    await Future.delayed(Duration(seconds: 2));
+   String catalogjson= await rootBundle.loadString("asset/catalo.json");
+
+   var decodeddata=jsonDecode(catalogjson);
+
+   var productsdata = decodeddata["product"];
+
+  Catalog.items= List.from(productsdata).map<Catalog>((Items) => Catalog.fromMap(Items)).toList();
+  print(Catalog.items);
+
+   setState(() {});
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
-    final products =[new Catalog('v', 'iPhone X', 'Apple iPhone 10th generation', 500, '33505a', "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQOfw0uRHkm28exE-W9S8dQCzMKqvYX-LwTRKjGv-o_Z0PvUn5AMedOq9Muuubde_1cyHKBDN76Yg2iHsn3328wUzZ6PX3C9UiHFppsCrFX5IL6NRuMfs2Pjw&usqp=CAE"),
-                     new Catalog("vishnu","iPhone 12 Pro Max", "Apple iPhone 12th generation", 1200, '33505a', 'https://img5.gadgetsnow.com/gd/images/products/additional/large/G201718_View_1/mobiles/smartphones/apple-iphone-12-pro-max-pacific-blue-128gb-.jpg'),
-                     new Catalog("Vishnu", "iPhone 12 Pro", 'Apple iPhone 12th generation',999 , '33505a', 'https://cdn.dxomark.com/wp-content/uploads/medias/post-61584/iphone-12-pro-max-graphite-hero-1024x768.jpg'),
-                     new Catalog("Vishnu1", "iPhone 12 ", 'Apple iPhone 12th generation', 899, '33505a', 'https://m.media-amazon.com/images/I/71hIfcIPyxS._SX679_.jpg'),
-                     new Catalog("google", "Pixel 3", "Pixel 3rd generation", 400, "33505a","https://m.media-amazon.com/images/I/718ynShMnpL._SX679_.jpg" ),
-                     new Catalog("vd", "Pixel 3a XL", "Pixel 3rd generation", 450, '33505a', "https://m.media-amazon.com/images/I/71WCmrY3Q+L._SX679_.jpg"),
-                     new Catalog('d', "Pixel 4", "Pixel 4th generation", 500, '33505a','https://m.media-amazon.com/images/I/61nJq44iMLL._SX679_.jpg' ),
-                     new Catalog('d', "Pixel 4a", "Pixel 4th generation", 480, '33505a','https://m.media-amazon.com/images/I/7199N-Uz2AL._SX679_.jpg' ),
-                     new Catalog('d', "Pixel 4 XL", "Pixel 4th generation", 580, '33505a','https://cdn.shopify.com/s/files/1/0598/7919/4823/products/71oTy_incwL._SS640.jpg?v=1664886375' ),
-                     new Catalog('d', "Pixel 6", "Pixel 6th generation", 600, '33505a',"https://m.media-amazon.com/images/I/61ruKkvVIxL._SX679_.jpg" ),
-                     new Catalog('d', "Pixel 6a", "Pixel 6th generation", 590, '33505a', "https://m.media-amazon.com/images/I/314423cfIVL.jpg"),
-                     new Catalog('d', "Pixel 6 Pro ", "Pixel 6th generation", 400, '33505a', "https://m.media-amazon.com/images/I/41x6M8sd69L._SX300_SY300_QL70_FMwebp_.jpg"),
-                     new Catalog('d', "OnePlus 6T ", "OnePlus 6th generation", 450, '33505a', "https://m.media-amazon.com/images/I/41fBRgy0ipL._SX300_SY300_QL70_FMwebp_.jpg"),
-                     new Catalog('d', "OnePlus 6 ", "OnePlus 6th generation", 390, '33505a', "https://m.media-amazon.com/images/I/81-btS1UL3L._SY741_.jpg"),
-                     new Catalog('d', "OnePlus 5 ", "OnePlus 5th generation", 350, '33505a', "https://m.media-amazon.com/images/I/41UiS3t+ptL._SY300_SX300_.jpg"),
-                     new Catalog('d', "OnePlus 8 ", "OnePlus 8th generation", 550, '33505a', "https://m.media-amazon.com/images/I/71KJ3loLvyL._SX679_.jpg"),
-                     new Catalog('d', "OnePlus 8T ", "OnePlus 8th generation", 590, '33505a', "https://m.media-amazon.com/images/I/41gsEWSkE1L._SX300_SY300_QL70_FMwebp_.jpg"),
-      new Catalog('v', 'iPhone X', 'Apple iPhone 10th generation', 500, '33505a', "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQOfw0uRHkm28exE-W9S8dQCzMKqvYX-LwTRKjGv-o_Z0PvUn5AMedOq9Muuubde_1cyHKBDN76Yg2iHsn3328wUzZ6PX3C9UiHFppsCrFX5IL6NRuMfs2Pjw&usqp=CAE"),
-      new Catalog("vishnu","iPhone 12 Pro Max", "Apple iPhone 12th generation", 1200, '33505a', 'https://img5.gadgetsnow.com/gd/images/products/additional/large/G201718_View_1/mobiles/smartphones/apple-iphone-12-pro-max-pacific-blue-128gb-.jpg'),
-      new Catalog("Vishnu", "iPhone 12 Pro", 'Apple iPhone 12th generation',999 , '33505a', 'https://cdn.dxomark.com/wp-content/uploads/medias/post-61584/iphone-12-pro-max-graphite-hero-1024x768.jpg'),
-      new Catalog("Vishnu1", "iPhone 12 ", 'Apple iPhone 12th generation', 899, '33505a', 'https://m.media-amazon.com/images/I/71hIfcIPyxS._SX679_.jpg'),
-      new Catalog("google", "Pixel 3", "Pixel 3rd generation", 400, "33505a","https://m.media-amazon.com/images/I/718ynShMnpL._SX679_.jpg" ),
-      new Catalog("vd", "Pixel 3a XL", "Pixel 3rd generation", 450, '33505a', "https://m.media-amazon.com/images/I/71WCmrY3Q+L._SX679_.jpg"),
-      new Catalog('d', "Pixel 4", "Pixel 4th generation", 500, '33505a','https://m.media-amazon.com/images/I/61nJq44iMLL._SX679_.jpg' ),
-      new Catalog('d', "Pixel 4a", "Pixel 4th generation", 480, '33505a','https://m.media-amazon.com/images/I/7199N-Uz2AL._SX679_.jpg' ),
-      new Catalog('d', "Pixel 4 XL", "Pixel 4th generation", 580, '33505a','https://cdn.shopify.com/s/files/1/0598/7919/4823/products/71oTy_incwL._SS640.jpg?v=1664886375' ),
-      new Catalog('d', "Pixel 6", "Pixel 6th generation", 600, '33505a',"https://m.media-amazon.com/images/I/61ruKkvVIxL._SX679_.jpg" ),
-      new Catalog('d', "Pixel 6a", "Pixel 6th generation", 590, '33505a', "https://m.media-amazon.com/images/I/314423cfIVL.jpg"),
-      new Catalog('d', "Pixel 6 Pro ", "Pixel 6th generation", 400, '33505a', "https://m.media-amazon.com/images/I/41x6M8sd69L._SX300_SY300_QL70_FMwebp_.jpg"),
-      new Catalog('d', "OnePlus 6T ", "OnePlus 6th generation", 450, '33505a', "https://m.media-amazon.com/images/I/41fBRgy0ipL._SX300_SY300_QL70_FMwebp_.jpg"),
-      new Catalog('d', "OnePlus 6 ", "OnePlus 6th generation", 390, '33505a', "https://m.media-amazon.com/images/I/81-btS1UL3L._SY741_.jpg"),
-      new Catalog('d', "OnePlus 5 ", "OnePlus 5th generation", 350, '33505a', "https://m.media-amazon.com/images/I/41UiS3t+ptL._SY300_SX300_.jpg"),
-      new Catalog('d', "OnePlus 8 ", "OnePlus 8th generation", 550, '33505a', "https://m.media-amazon.com/images/I/71KJ3loLvyL._SX679_.jpg"),
-      new Catalog('d', "OnePlus 8T ", "OnePlus 8th generation", 590, '33505a', "https://m.media-amazon.com/images/I/41gsEWSkE1L._SX300_SY300_QL70_FMwebp_.jpg"),
-
-    ];
     return Scaffold(
-      appBar: AppBar( title:Text("Catalog App",style: TextStyle(color: Colors.black87),),backgroundColor: Colors.white,iconTheme: IconThemeData(color: Colors.black),),
+      appBar: AppBar(
+        title: Text("Catalog App", style: TextStyle(color: Colors.black87),),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(itemCount: products.length, itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(leading: Image.network(products[index].imageurl,height: 50,width: 50,),
-                    title: Text(products[index].name),
-                    subtitle: Text(products[index].descprition),
-                     trailing: Text("\$${products[index].price}"),),
-          );
-        }, ),
+        child: (Catalog.items.isNotEmpty)?GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 10,mainAxisSpacing: 10),
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: GridTile(header: SizedBox(height: 100,width: 12,child: Text("gdf"),),child: Image.network(Catalog.items[index].imageurl,height: 1,width: 1,),),
+            );
+          },itemCount: Catalog.items.length,):Center(child: CircularProgressIndicator(),),
       ),
       drawer: MyDrawer(),
     );
   }
-
 
 }
